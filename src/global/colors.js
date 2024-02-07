155b7fcb661dd4718e2a18686e39c7dba8a38edb
hxxtae <fkdlxmfkdl@gmail.com>
2024-02-07 23:28:40 +0900

    :books: [Docs]: Add colors

---
+++ src/global/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  background: '#0d1b2a',
+  text: '#ffffff',
+  first: '#1b263b',
+  secondary: '#415a77',
+  tertiary: '#778da9',
+  foreground: '#e0e1dd',
+  red: '#E71F1F',
+  green: '#3FC300',
+  blue: '#0085FF',
+}
