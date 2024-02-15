b5dab889fd6ecf61f2c098355f02b5823509a10a
hxxtae <fkdlxmfkdl@gmail.com>
2024-02-15 22:50:22 +0900

    :recycle: [refactory]: Fix LedComponent
    - add component of LedPower

--- src/page/LedComponent/index.jsx
+++ src/page/LedComponent/index.jsx
@@ -1,30 +1,15 @@
-import { ScrollView, TouchableOpacity, View } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { ScrollView } from 'react-native';
 
-import { colors } from '../../global/colors';
 import { styles as S } from './styles';
+import LedPower from './LedPower';
 import LedTotal from './LedTotal';
 import LedDetail from './LedDetail';
-import { useState } from 'react';
 
 export default function LedComponent() {
-  const [power, setPower] = useState(true);
-
-  const handlePowerState = () => {
-    setPower((prev) => !prev);
-  }
 
   return (
     <ScrollView style={S.container}>
-      <View style={S.topBtnBox}>
-        <TouchableOpacity style={[S.circleBtn]}>
-          <Feather name="bluetooth" size={24} color={colors.text} />
-        </TouchableOpacity>
-        <TouchableOpacity style={[S.circleBtn, S.powerBtn(power)]} onPress={handlePowerState}>
-          <Feather name="power" size={24} color={power ? colors.green : colors.text} />
-        </TouchableOpacity>
-      </View>
-
+      <LedPower />
       <LedTotal />
       <LedDetail />
     </ScrollView>
--- src/page/LedComponent/styles.js
+++ src/page/LedComponent/styles.js
@@ -7,19 +7,4 @@ export const styles = StyleSheet.create({
     paddingTop: 20,
     backgroundColor: colors.background,
   },
-  // 1. Top Buttons
-  topBtnBox: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    gap: 20,
-  },
-  circleBtn: {
-    padding: 10,
-    borderRadius: 100,
-    backgroundColor: colors.first,
-  },
-  powerBtn: (state) => ({
-    backgroundColor: state ? colors.foreground : colors.first,
-  })
 })
